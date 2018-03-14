0a4083f94cf2a163ec0e7107e39e25a665e5c52e
JAreina <jareinafdez@gmail.com>
2018-03-14 20:01:43 +0100

    ROUTE PARAMS

--- servicios-routing/src/app/app.module.ts
+++ servicios-routing/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 /* */ import { Routes, RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProveedoresService } from './servicios/proveedores.service';
@@ -11,11 +11,15 @@ import { MiServicioService } from './servicios/mi-servicio.service';
 import { NotFoundComponent } from './not-found/not-found/not-found.component';
 import { HeaderComponent } from './header/header.component';
 import { BorrarComponent } from './borrar/borrar.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'proveedores', component: ProveedoresComponent },
   { path: 'borrar', component: BorrarComponent },
+  { path: 'contacto/:id', component: ContactoComponent},
+  { path: 'contacto/:id/:nombre', component: ContactoComponent},
+  { path: 'contacto', component: ContactoComponent},
   { path: '**', component: NotFoundComponent} /* Cambiar por 404 */
 ];
 
@@ -26,10 +30,12 @@ const routes: Routes = [
     InicioComponent,
     NotFoundComponent,
     HeaderComponent,
-    BorrarComponent
+    BorrarComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ProveedoresService, MiServicioService],
