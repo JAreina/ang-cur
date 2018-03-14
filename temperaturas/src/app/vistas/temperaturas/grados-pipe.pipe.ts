fd5ba0ca08481f119f665f7e3fcf297e1aba2f7d
JAreina <jareinafdez@gmail.com>
2018-03-14 15:31:05 +0100

    pipe personalizado

--- temperaturas/src/app/vistas/temperaturas/grados-pipe.pipe.ts
+++ temperaturas/src/app/vistas/temperaturas/grados-pipe.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class GradosPipePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform(value: any): any {
     const grados = `${value} º`;
     return grados;
   }
